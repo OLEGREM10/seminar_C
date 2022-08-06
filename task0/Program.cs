d464e31d3ace4d15cb8c3a668a7cb93a16bc6575
Oleg <olegrem1020@mail.ru>
2022-08-06 23:19:59 +0600

    add .gitignore and task0_1_2

---
+++ task0/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.Write("Введите число "); // 1. предлогаем пользователю ввести число
+//int number = Convert.ToInt32(Console.ReadLine()); // 2.  Считывание строковых данных и преоброзование в тип int
+string? str = Console.ReadLine(); // 2.1 Считываем данные введеные пользователем с консоли
+int number = Convert.ToInt32(str); // 2.2 конвертируем из строкового типа в целочисленый тип
+Console.Write(number * number); // 3.Вывод на экран квадрат числа,number ^ 2
+
+
+
